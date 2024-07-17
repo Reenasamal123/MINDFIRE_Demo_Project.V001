998a821fa3c4aa2f37cecfab3460263aafa9b24e
Reenasamal123 <mfsi.reena@gmail.com>
2024-07-17 15:44:07 +0530

    this is my first commit

---
+++ src/main/java/MINDFIRE_Demo_Project/MINDFIRE_Demo_Project/App.java
@@ -0,0 +1,13 @@
+package MINDFIRE_Demo_Project.MINDFIRE_Demo_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Pages/Home.java
@@ -0,0 +1,11 @@
+package Pages;
+
+public class Home {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to Jenkins");
+
+	}
+
+}
